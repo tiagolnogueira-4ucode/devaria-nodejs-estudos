ff72bd49e3e78e9c5aa6e09abb64de9fa2562fff
tiagolnogueira-4ucode <tiagol.nogueira@gmail.com>
2023-09-07 21:04:19 -0300

    variaveis e input

---
+++ variaveis-e-input/app.js
@@ -0,0 +1,16 @@
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const minhaPrimeiraConstanteString = 'Minha primeira constante';
+console.log(minhaPrimeiraConstanteString);
+
+let leituraDeCampo;
+
+readLine.question('Por favor digite alguma coisa: ', input => {
+    leituraDeCampo = input;
+    console.log(`O usuário digitou: ${leituraDeCampo}`);
+});
+
+
